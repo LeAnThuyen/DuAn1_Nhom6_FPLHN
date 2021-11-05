12efd5ac66cc0f93cd2acdf44cc7365a4cbf1644
Tana <leanthuyen08122002@gmail.com>
2021-11-05 22:28:10 +0700

    chatlieu

---
+++ 1_DAL_DataAccessLayer/IDALServices/IChatLieuServices.cs
@@ -0,0 +1,18 @@
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.IDALServices
+{
+    public interface IChatLieuServices
+    {
+
+        List<ChatLieu> getlstchatlieufromDB();
+        bool addchatlieu(ChatLieu cl);
+        bool deletechatlieu(ChatLieu cl);
+        bool update(ChatLieu cl);
+    }
+}
