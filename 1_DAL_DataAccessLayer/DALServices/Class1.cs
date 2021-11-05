f5b4703c6bded5ff07dbb02aefc5f03008bd62f8
Tana <leanthuyen08122002@gmail.com>
2021-11-05 15:48:25 +0700

    abc

---
+++ 1_DAL_DataAccessLayer/DALServices/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.DALServices
+{
+    class Class1
+    {
+        public int abc { get; set; }
+    }
+}
