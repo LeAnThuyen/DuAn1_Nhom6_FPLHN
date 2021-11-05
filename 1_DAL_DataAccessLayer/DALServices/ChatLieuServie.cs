153d586ef73945288097bf1cdd016c988d188827
Tana <leanthuyen08122002@gmail.com>
2021-11-05 22:52:25 +0700

    lenluon

---
+++ 1_DAL_DataAccessLayer/DALServices/ChatLieuServie.cs
@@ -0,0 +1,49 @@
+﻿using _1_DAL_DataAccessLayer.Context;
+using _1_DAL_DataAccessLayer.IDALServices;
+using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.DALServices
+{
+    public class ChatLieuServie : IChatLieuServices
+    {
+        private DatabaseContext _DBcontext;
+        private List<ChatLieu> _lstchatlieu;
+        public ChatLieuServie()
+        {
+            _DBcontext = new DatabaseContext();
+            _lstchatlieu = new List<ChatLieu>();
+        }
+        public bool addchatlieu(ChatLieu cl)
+        {
+            _DBcontext.ChatLieus.Add(cl);
+            _DBcontext.SaveChanges();
+            return true;
+        }
+
+        public bool deletechatlieu(ChatLieu cl)
+        {
+            _DBcontext.ChatLieus.Remove(cl);
+            _DBcontext.SaveChanges();
+            return true;
+        }
+
+        public List<ChatLieu> getlstchatlieufromDB()
+        {
+            _lstchatlieu = _DBcontext.ChatLieus.ToList();
+            return _lstchatlieu;
+        }
+
+        public bool update(ChatLieu cl)
+        {
+
+            _DBcontext.ChatLieus.Remove(cl);
+            _DBcontext.SaveChanges();
+            return true;
+        }
+    }
+}
