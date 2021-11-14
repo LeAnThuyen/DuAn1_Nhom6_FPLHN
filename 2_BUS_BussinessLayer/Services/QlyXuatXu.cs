58eb3f6f80ebb695490606cce7bceab9d154f3dd
thanhthangabc <93759235+thanhthangabc@users.noreply.github.com>
2021-11-14 23:01:34 +0700

    a

---
+++ 2_BUS_BussinessLayer/Services/QlyXuatXu.cs
@@ -0,0 +1,63 @@
+﻿using _1_DAL_DataAccessLayer.DALServices;
+using _1_DAL_DataAccessLayer.IDALServices;
+using _1_DAL_DataAccessLayer.Models;
+using _2_BUS_BussinessLayer.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS_BussinessLayer.Services
+{
+
+    public class QlyXuatXu : IQlyXuatXu
+    {
+        private XuatXuService _xx;
+        
+        private List<XuatXu> _lstxuatXu;
+        public QlyXuatXu()
+        {
+            _xx = new XuatXuService();
+      
+            _lstxuatXu = new List<XuatXu>();
+
+        
+            
+           // GetsList();
+        }
+        public bool addXX(XuatXu xuatXu)
+        {
+            _xx.addxuatxu(xuatXu);
+            
+            return true;
+        }
+
+        public List<XuatXu> GetsList()
+        {
+            _lstxuatXu= _xx.getlstxuatxufromDB();
+            return _lstxuatXu;
+
+        }
+
+        public bool removeXX(XuatXu xuatXu)
+        {
+            _xx.deletexuatxu(xuatXu);
+
+            return true;
+        }
+
+        public void Save(XuatXu xuatXu)
+        {
+            _xx.save(xuatXu);
+
+            
+        }
+
+        public bool updateXX(XuatXu xuatXu)
+        {
+            _xx.updatexuatxu(xuatXu);
+            return true;
+        }
+    }
+}
