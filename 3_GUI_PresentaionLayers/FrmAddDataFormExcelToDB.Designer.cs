418fc8726b9074bef59d979c20255d161073c3c0
Tana <leanthuyen08122002@gmail.com>
2021-11-14 23:18:55 +0700

    ok

---
+++ 3_GUI_PresentaionLayers/FrmAddDataFormExcelToDB.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace _3_GUI_PresentaionLayers
+{
+    partial class FrmAddDataFormExcelToDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAddDataFormExcelToDB));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_filename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1924, 444);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txt_filename
+            // 
+            this.txt_filename.Location = new System.Drawing.Point(82, 451);
+            this.txt_filename.Name = "txt_filename";
+            this.txt_filename.ReadOnly = true;
+            this.txt_filename.Size = new System.Drawing.Size(1742, 27);
+            this.txt_filename.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(0, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1830, 450);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 46);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1830, 514);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(94, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1830, 578);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(94, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(1830, 654);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(94, 46);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // FrmAddDataFormExcelToDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(1924, 712);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_filename);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmAddDataFormExcelToDB";
+            this.Text = "FrmAddDataFormExcelToDB";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_filename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
---
+++ 3_GUI_PresentaionLayers/FrmAddDataFormExcelToDB.cs
@@ -0,0 +1,236 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using _2_BUS_BussinessLayer.Services;
+using _1_DAL_DataAccessLayer.Models;
+
+namespace _3_GUI_PresentaionLayers
+{
+    public partial class FrmAddDataFormExcelToDB : Form
+    {
+
+        private QlyHangHoaServices qlyHangHoaServices;
+        private string mahh = "";
+        private int id;
+        private int idcthh;
+        private string tenhh = "";
+        private int idnsx;
+        private int iddanhmuc;
+        private int trangthai;
+        private string mavach = "";
+        private string soluong = "";
+        private double gianhap;
+        private double giaban;
+        private DateTime? ngaynhap;
+        private int idtenchatlieu;
+        private int idtenvatchua;
+        private int idnhomhuong;
+        private int idhh;
+        private int idtennquocgia;
+        private int idsodungtich;
+        private int idanh;
+        private DateTime? hsd;
+        private string model = "";
+        public FrmAddDataFormExcelToDB()
+        {
+            InitializeComponent();
+            qlyHangHoaServices = new QlyHangHoaServices();
+
+        }
+        public void Alert(string mess)
+        {
+            FrmAlert frmAlert = new FrmAlert();
+            frmAlert.showAlert(mess);
+        }
+        public void AlertErr(string mess)
+        {
+            FrmThatBaiAlert frmThatBaiAlert = new FrmThatBaiAlert();
+            frmThatBaiAlert.showAlert(mess);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select File";
+            openFileDialog.FileName = txt_filename.Text;
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txt_filename.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn Có Muốn Import File Excel Lên Hay Không ?", "Thông Báo", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                var conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + txt_filename.Text + "';Extended Properties=Excel 12.0 Xml;");
+                conn.Open();
+                OleDbDataAdapter dataAdapter = new OleDbDataAdapter("Select * from[Sheet1$]", conn);
+                DataSet theSD = new DataSet();
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                this.dataGridView1.DataSource = dt.DefaultView;
+                for (int i = 0; i < 2; i++)
+                {
+                    this.Alert("Import Thành Công Rực Rỡ");
+                }
+                return;
+            };
+
+            if (dialogResult == DialogResult.No)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    this.AlertErr("Import Thất Bại");
+                }
+                return;
+            }
+
+            //dataGridView1.Columns[0].Visible = false;//idhh
+            //dataGridView1.Columns[6].Visible = false;//idcthh
+            //dataGridView1.Columns[3].Visible = false;//idcthh
+            //dataGridView1.Columns[4].Visible = false;//idcthh
+            //dataGridView1.Columns[14].Visible = false;//idcthh
+            //dataGridView1.Columns[15].Visible = false;//idcthh
+            //dataGridView1.Columns[16].Visible = false;//idcthh
+            //dataGridView1.Columns[17].Visible = false;//idcthh
+            //dataGridView1.Columns[18].Visible = false;//idcthh
+            //dataGridView1.Columns[19].Visible = false;//idcthh
+            //dataGridView1.Columns[20].Visible = false;//idcthh
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn Có Muốn Thêm Các Sản Phẩm Ở File Excel Vào Database Hay Không ?", "Thông Báo", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    //hh
+                    id = Convert.ToInt32(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    tenhh = Convert.ToString(dataGridView1.Rows[i].Cells[1].Value);
+                    mahh = Convert.ToString(dataGridView1.Rows[i].Cells[2].Value);
+                    idnsx = Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value);
+                    trangthai = Convert.ToInt32(dataGridView1.Rows[i].Cells[4].Value);
+                    iddanhmuc = Convert.ToInt32(dataGridView1.Rows[i].Cells[5].Value);
+                    //cthh
+                    idcthh = Convert.ToInt32(dataGridView1.Rows[i].Cells[6].Value);
+                    soluong = Convert.ToString(dataGridView1.Rows[i].Cells[7].Value);
+                    mavach = Convert.ToString(dataGridView1.Rows[i].Cells[8].Value);
+                    gianhap = Convert.ToDouble(dataGridView1.Rows[i].Cells[9].Value);
+                    giaban = Convert.ToDouble(dataGridView1.Rows[i].Cells[10].Value);
+                    hsd = Convert.ToDateTime(dataGridView1.Rows[i].Cells[11].Value);
+                    model = Convert.ToString(dataGridView1.Rows[i].Cells[12].Value);
+                    ngaynhap = Convert.ToDateTime(dataGridView1.Rows[i].Cells[13].Value);
+                    trangthai = Convert.ToInt32(dataGridView1.Rows[i].Cells[14].Value);
+                    idtenchatlieu = Convert.ToInt32(dataGridView1.Rows[i].Cells[15].Value);
+                    idnhomhuong = Convert.ToInt32(dataGridView1.Rows[i].Cells[16].Value);
+                    idhh = Convert.ToInt32(dataGridView1.Rows[i].Cells[17].Value);
+                    idtennquocgia = Convert.ToInt32(dataGridView1.Rows[i].Cells[18].Value);
+                    idsodungtich = Convert.ToInt32(dataGridView1.Rows[i].Cells[19].Value);
+                    idanh = Convert.ToInt32(dataGridView1.Rows[i].Cells[20].Value);
+                    idtenvatchua = Convert.ToInt32(dataGridView1.Rows[i].Cells[21].Value);
+
+
+                    var sp = new HangHoa()
+                    {
+                        IdhangHoa = id,
+                        TenHangHoa = tenhh,
+                        MaHangHoa = mahh,
+                        IdnhaSanXuat = idnsx,
+                        TrangThai = trangthai,
+                        IddanhMuc=iddanhmuc
+                    };
+                    qlyHangHoaServices.AddSP(sp);
+                 //   qlyHangHoaServices.SaveHangHoa(sp);
+
+
+                    var info = new ChiTietHangHoa()
+                    {
+
+                        IdthongTinHangHoa = idcthh,
+                        SoLuong = soluong,
+                        Mavach = mavach,
+                        DonGiaNhap = gianhap,
+                        DonGiaBan = giaban,
+                        HanSuDung = hsd,
+                        Model = model,
+                        NgayNhapKho = ngaynhap,
+                        TrangThai = trangthai,
+                        IdchatLieu = idtenchatlieu,
+                        IdnhomHuong = idnhomhuong,
+                        IdhangHoa = idhh,
+                        IdquocGia = idtennquocgia,
+                        IddungTich = idsodungtich,
+                        Idanh = idanh,
+                        IdvatChua = idtenvatchua,
+                    };
+                    qlyHangHoaServices.AddSpChiTiet(info);
+                  //  qlyHangHoaServices.SaveChiTietHangHoa(info);
+                };
+
+                for (int i = 0; i < 2; i++)
+                {
+                    this.Alert("Thêm Thành Công");
+                }
+                return;
+
+
+
+            };
+                if (dialogResult == DialogResult.No)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        this.AlertErr("Thêm Thất Bại");
+                    }
+                    return;
+                }
+            }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Bạn Có Muốn Thoát Hay Không ?", "Thông Báo", MessageBoxButtons.YesNo);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                FormSanPham formSanPham = new FormSanPham();
+
+                formSanPham.loaddata();
+                FrmLogin frmLogin = new FrmLogin();
+                frmLogin.Show();
+                this.Close();
+                for (int i = 0; i < 2; i++)
+                {
+                    this.Alert("Chào Mừng Bạn Đến Với PerSoft Perfume <3");
+
+                }
+                return;
+            };
+
+            if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+    }
+    }
+
