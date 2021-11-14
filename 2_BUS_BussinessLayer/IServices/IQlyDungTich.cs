77671840757279c1d20f17bd67b6946cd9255303
thanhthangabc <93759235+thanhthangabc@users.noreply.github.com>
2021-11-14 23:00:04 +0700

    a

---
+++ 2_BUS_BussinessLayer/IServices/IQlyDungTich.cs
@@ -0,0 +1,18 @@
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS_BussinessLayer.IServices
+{
+    public interface IQlyDungTich
+    {
+        bool addDT(DungTich dungTich);
+        bool removeDT(DungTich dungTich);
+        bool updateDT(DungTich dungTich);
+        List<DungTich> GetsList();
+        void Save(DungTich dungTich);
+    }
+}
