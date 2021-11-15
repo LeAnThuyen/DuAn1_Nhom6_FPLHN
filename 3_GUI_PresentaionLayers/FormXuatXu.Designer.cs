fa08a2f3e088e3d0dcaeeee3d5f1af43271adfa1
Tana <leanthuyen08122002@gmail.com>
2021-11-15 22:25:45 +0700

    q

---
+++ 3_GUI_PresentaionLayers/FormXuatXu.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace _3_GUI_PresentaionLayers
+{
+    partial class FormXuatXu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxTenQuocGia = new System.Windows.Forms.ComboBox();
+            this.txtMaQuocGia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv_XuatXu = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_XuatXu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxTenQuocGia
+            // 
+            this.cbxTenQuocGia.FormattingEnabled = true;
+            this.cbxTenQuocGia.Location = new System.Drawing.Point(279, 90);
+            this.cbxTenQuocGia.Margin = new System.Windows.Forms.Padding(2);
+            this.cbxTenQuocGia.Name = "cbxTenQuocGia";
+            this.cbxTenQuocGia.Size = new System.Drawing.Size(146, 28);
+            this.cbxTenQuocGia.TabIndex = 13;
+            // 
+            // txtMaQuocGia
+            // 
+            this.txtMaQuocGia.Location = new System.Drawing.Point(279, 38);
+            this.txtMaQuocGia.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaQuocGia.Name = "txtMaQuocGia";
+            this.txtMaQuocGia.Size = new System.Drawing.Size(146, 27);
+            this.txtMaQuocGia.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Tên Quốc Gia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Mã Quốc Gia";
+            // 
+            // dgv_XuatXu
+            // 
+            this.dgv_XuatXu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_XuatXu.Location = new System.Drawing.Point(26, 132);
+            this.dgv_XuatXu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv_XuatXu.Name = "dgv_XuatXu";
+            this.dgv_XuatXu.RowHeadersWidth = 62;
+            this.dgv_XuatXu.RowTemplate.Height = 33;
+            this.dgv_XuatXu.Size = new System.Drawing.Size(540, 180);
+            this.dgv_XuatXu.TabIndex = 25;
+            this.dgv_XuatXu.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_XuatXu_CellClick_1);
+            this.dgv_XuatXu.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_XuatXu_CellContentClick_1);
+            // 
+            // FormXuatXu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.ClientSize = new System.Drawing.Size(602, 345);
+            this.Controls.Add(this.dgv_XuatXu);
+            this.Controls.Add(this.cbxTenQuocGia);
+            this.Controls.Add(this.txtMaQuocGia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "FormXuatXu";
+            this.Text = "XuatXu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_XuatXu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxTenQuocGia;
+        private System.Windows.Forms.TextBox txtMaQuocGia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgv_XuatXu;
+    }
+}
---
+++ 3_GUI_PresentaionLayers/FormXuatXu.cs
@@ -0,0 +1,187 @@
+﻿using _1_DAL_DataAccessLayer.DALServices;
+using _1_DAL_DataAccessLayer.Models;
+using _2_BUS_BussinessLayer.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_GUI_PresentaionLayers
+{
+    public partial class FormXuatXu : Form
+    {
+        private QlyXuatXu xuatXu;
+        private XuatXu _xuatXu;
+        private XuatXuService _xuatXuSer;
+        public FormXuatXu()
+        {
+            InitializeComponent();
+            xuatXu = new QlyXuatXu();
+            _xuatXu = new XuatXu();
+            _xuatXuSer = new XuatXuService();
+            _xuatXuSer.getlstxuatxufromDB();
+            dgv_XuatXu.AllowUserToAddRows = false;
+            loaddata();
+        }
+        void loaddata()
+        {
+            ArrayList row = new ArrayList();
+
+            row = new ArrayList();
+            row.Add("Thêm");
+            row.Add("Sửa");
+            row.Add("Xóa");
+
+            dgv_XuatXu.ColumnCount = 4;
+            dgv_XuatXu.Columns[0].Name = "ID Quoc Gia";
+            dgv_XuatXu.Columns[0].Visible = false;
+            dgv_XuatXu.Columns[1].Name = "Mã Quốc Gia";
+            dgv_XuatXu.Columns[2].Name = "Tên Quốc Gia";
+            dgv_XuatXu.Columns[3].Name = "Trạng thái";
+            DataGridViewComboBoxColumn cbo = new DataGridViewComboBoxColumn();
+            cbo.HeaderText = "Chức Năng";
+            cbo.Name = "cbo";
+            cbo.Items.AddRange(row.ToArray());
+            dgv_XuatXu.Columns.Add(cbo);
+
+
+            ////button
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.Text = "Xác Nhận";
+            btn.HeaderText = "Xác Nhận";
+            btn.Name = "btn";
+            btn.UseColumnTextForButtonValue = true;
+            dgv_XuatXu.Columns.Add(btn);
+
+
+
+            dgv_XuatXu.Rows.Clear();
+            foreach (var x in xuatXu.GetsList())
+            {
+                dgv_XuatXu.Rows.Add(x.IdquocGia, x.MaQuocGia, x.TenQuocGia, x.TrangThai == 1 ? "Tồn tại" : "Không tồn tại");
+            }
+        }
+
+        private void dgv_XuatXu_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dgv_XuatXu_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void dgv_XuatXu_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowindex = e.RowIndex;
+            if (rowindex == xuatXu.GetsList().Count || rowindex == -1) return;
+            _xuatXu = _xuatXuSer.getlstxuatxufromDB().ToList().FirstOrDefault(c => c.IdquocGia == Convert.ToInt32(dgv_XuatXu.Rows[rowindex].Cells[0].Value.ToString()));
+            txtMaQuocGia.Text = dgv_XuatXu.Rows[rowindex].Cells[1].Value.ToString();
+            cbxTenQuocGia.Text = dgv_XuatXu.Rows[rowindex].Cells[2].Value.ToString();
+        }
+
+        private void dgv_XuatXu_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int rd = e.RowIndex;
+            dgv_XuatXu.AllowUserToAddRows = false;
+
+
+            //thêm
+            if (e.ColumnIndex == 5 && string.IsNullOrEmpty(dgv_XuatXu.Rows[rd].Cells["cbo"].Value.ToString()) == false)
+            {
+
+                string commnad = dgv_XuatXu.Rows[e.RowIndex].Cells["cbo"].Value.ToString();
+
+                if (commnad == "Thêm")
+                {
+                    DialogResult dialogResult = MessageBox.Show("bạn có muốn chọn chức năng Thêm hay không", "Thông Báo", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+
+                        _xuatXuSer.addxuatxu(new XuatXu()
+                        {
+                            IdquocGia = _xuatXuSer.getlstxuatxufromDB().Max(c => c.IdquocGia) + 1,
+                            MaQuocGia = txtMaQuocGia.Text,
+                            TenQuocGia = cbxTenQuocGia.Text
+
+
+                        });
+                        _xuatXuSer.save(_xuatXu);
+                        MessageBox.Show("Thêm Thành Công", "Thông Báo");
+                        _xuatXuSer.getlstxuatxufromDB();
+                        loaddata();
+                        return;
+                    }
+                    if (dialogResult == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+
+
+            }
+            //sửa
+            if (e.ColumnIndex == 5 && string.IsNullOrEmpty(dgv_XuatXu.Rows[rd].Cells["cbo"].Value.ToString()) == false)
+            {
+
+                string commnad = dgv_XuatXu.Rows[e.RowIndex].Cells["cbo"].Value.ToString();
+
+                if (commnad == "Sửa")
+                {
+                    DialogResult dialogResult = MessageBox.Show("bạn có muốn chọn chức năng Sửa hay không", "Thông Báo", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+
+                        _xuatXu.IdquocGia = Convert.ToInt32(dgv_XuatXu.Rows[e.RowIndex].Cells[0].Value.ToString());
+                        _xuatXu.MaQuocGia = txtMaQuocGia.Text;
+                        _xuatXu.TenQuocGia = cbxTenQuocGia.Text;
+
+
+                        xuatXu.updateXX(_xuatXu);
+                        xuatXu.Save(_xuatXu);
+                        MessageBox.Show("Sửa Thành Công", "Thông Báo");
+                        _xuatXuSer.getlstxuatxufromDB();
+                        loaddata();
+                        return;
+                    }
+                    if (dialogResult == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+
+            }
+            //xóa
+            if (e.ColumnIndex == 5 && string.IsNullOrEmpty(dgv_XuatXu.Rows[rd].Cells["cbo"].Value.ToString()) == false)
+            {
+
+                string commnad = dgv_XuatXu.Rows[e.RowIndex].Cells["cbo"].Value.ToString();
+                if (commnad == "Xóa")
+                {
+                    DialogResult dialogResult = MessageBox.Show("bạn có muốn chọn chức năng Sửa hay không", "Thông Báo", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        _xuatXuSer.deletexuatxu(_xuatXu);
+                        _xuatXuSer.save(_xuatXu);
+                        MessageBox.Show("Xóa Thành Công");
+                        _xuatXuSer.getlstxuatxufromDB();
+                        loaddata();
+
+                        return;
+                    }
+                    if (dialogResult == DialogResult.No)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
