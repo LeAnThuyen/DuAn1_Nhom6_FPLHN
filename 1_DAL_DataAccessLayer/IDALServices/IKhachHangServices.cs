a59fd58eefc23677c8357d899172838111715bab
Tana <leanthuyen08122002@gmail.com>
2021-11-16 15:22:13 +0700

    ok

---
+++ 1_DAL_DataAccessLayer/IDALServices/IKhachHangServices.cs
@@ -0,0 +1,19 @@
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.IDALServices
+{
+  public interface IKhachHangServices
+    {
+
+        List<KhachHang> getlstnhanvien();
+        bool addkhachhang(KhachHang kh);
+        bool updatekhachhang(KhachHang kh);
+        bool deletekhachhang(KhachHang kh);
+        bool savekhachhang(KhachHang kh);
+    }
+}
---
+++ 1_DAL_DataAccessLayer/IDALServices/INhanVienServices.cs
@@ -0,0 +1,19 @@
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.IDALServices
+{
+  public interface INhanVienServices
+    {
+        List<NhanVien> getlstnhanvien();
+        bool addnhanvien(NhanVien nv);
+        bool updatenhanvien(NhanVien nv);
+        bool deletenhanvien(NhanVien nv);
+        bool savenhanvien(NhanVien nv);
+        
+    }
+}
