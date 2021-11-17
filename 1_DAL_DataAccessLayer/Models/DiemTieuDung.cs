4f7734faa463f2cdba31183abd416b337a64becc
Tana <leanthuyen08122002@gmail.com>
2021-11-17 16:37:16 +0700

    ok

--- 1_DAL_DataAccessLayer/Models/DiemTieuDung.cs
+++ 1_DAL_DataAccessLayer/Models/DiemTieuDung.cs
@@ -11,22 +11,24 @@ namespace _1_DAL_DataAccessLayer.Models
     [Table("DiemTieuDung")]
     public partial class DiemTieuDung
     {
-        public DiemTieuDung()
-        {
-            KhachHangs = new HashSet<KhachHang>();
-        }
-
         [Key]
         [Column("IDDiemTieuDung")]
         public int IddiemTieuDung { get; set; }
-        [StringLength(50)]
-        public string MaDiem { get; set; }
-        [StringLength(50)]
-        public string TenDiem { get; set; }
         public double? SoDiem { get; set; }
         public int? TrangThai { get; set; }
+        [Column("IDLichSuDiem")]
+        public int? IdlichSuDiem { get; set; }
 
+        [ForeignKey(nameof(IdlichSuDiem))]
+        [InverseProperty(nameof(LichSuTieuDungDiem.DiemTieuDungs))]
+        public virtual LichSuTieuDungDiem IdlichSuDiemNavigation { get; set; }
         [InverseProperty(nameof(KhachHang.IddiemTieuDungNavigation))]
         public virtual ICollection<KhachHang> KhachHangs { get; set; }
+
     }
+
+
+
+
 }
+
