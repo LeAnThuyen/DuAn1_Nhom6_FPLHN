588ac85d94a2168cd42deb7b1101294c3e222a3a
Tana <leanthuyen08122002@gmail.com>
2021-11-18 03:36:06 +0700

    ok

--- 1_DAL_DataAccessLayer/Models/LichSuTieuDungDiem.cs
+++ 1_DAL_DataAccessLayer/Models/LichSuTieuDungDiem.cs
@@ -8,15 +8,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace _1_DAL_DataAccessLayer.Models
 {
-
     [Table("LichSuTieuDungDiem")]
     public partial class LichSuTieuDungDiem
     {
-        public LichSuTieuDungDiem()
-        {
-            DiemTieuDungs = new HashSet<DiemTieuDung>();
-        }
-
         [Key]
         [Column("IDLichSuDiem")]
         public int IdlichSuDiem { get; set; }
@@ -26,11 +20,14 @@ namespace _1_DAL_DataAccessLayer.Models
         public int? TrangThai { get; set; }
         [Column("IDHoaDon")]
         public int? IdhoaDon { get; set; }
+        [Column("IDDiemTieuDung")]
+        public int? IddiemTieuDung { get; set; }
 
+        [ForeignKey(nameof(IddiemTieuDung))]
+        [InverseProperty(nameof(DiemTieuDung.LichSuTieuDungDiems))]
+        public virtual DiemTieuDung IddiemTieuDungNavigation { get; set; }
         [ForeignKey(nameof(IdhoaDon))]
         [InverseProperty(nameof(HoaDonBan.LichSuTieuDungDiems))]
         public virtual HoaDonBan IdhoaDonNavigation { get; set; }
-        [InverseProperty(nameof(DiemTieuDung.IdlichSuDiemNavigation))]
-        public virtual ICollection<DiemTieuDung> DiemTieuDungs { get; set; }
     }
 }
