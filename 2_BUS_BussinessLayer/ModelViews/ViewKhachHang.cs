3043a3ec8a86dfb105acf4e20c5c9a7754ef70ed
Hoang Chung <chunghtbph16758@fpt.edu.vn>
2021-11-22 22:04:15 +0700

    update

---
+++ 2_BUS_BussinessLayer/ModelViews/ViewKhachHang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1_DAL_DataAccessLayer.Models;
+
+namespace _2_BUS_BussinessLayer.ModelViews
+{
+    public class ViewKhachHang
+    {
+        public KhachHang KhachHang { get; set; }
+        public DiemTieuDung DiemTieuDung { get; set; }
+        public LichSuTieuDungDiem LichSuTieuDungDiem { get; set; }
+    }
+}
